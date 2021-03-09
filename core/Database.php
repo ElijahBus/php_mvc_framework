3502c96b462ef4f8c932ace803b098671bc2193f
ElijahBus <elijahbus.ny@gmail.com>
2021-03-09 17:32:28 +0200

    feat(migrations) : create connection to mysql database

---
+++ core/Database.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\core;
+
+class Database
+{
+    public function __construct()
+    {
+        $this->pdo = new \PDO($dsn, $username, $password);
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+}
---
+++ core/migrations/m01_initial.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\core\migrations;
+
+class m01_initial
+{
+    public function up()
+    {
+        echo "Applying migrations";
+    }
+
+    public function down()
+    {
+        echo "Down migrations";
+    }
+}
